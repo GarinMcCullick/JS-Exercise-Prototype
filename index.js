5471bb8f814b187be1c7fdb34bd63af8284ec2f9
Garin Mccullick <glmccullick@gmail.com>
2021-03-25 01:09:56 -0500

    Prototypes

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function Airplane(name) {
   
   /*
     TASK 1
-      - Write a Person Constructor that initializes `name` and `age` from arguments.
+      - Write a Person Constructor that initializes `name` and `age` from arguments. done!
       - All instances of Person should initialize with an empty `stomach` array.
       - Give instances of Person the ability to `.eat("someFood")`:
           + When eating an edible, it should be pushed into the `stomach`.
@@ -39,11 +39,30 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
- 
- 
+
+   const personMary = new Person("mary", 50);
+   console.log (personMary);
+
+
+Person.prototype.eat = function(){
+    const someFood = [].lenght <= 10;//pushing an array to another array?
+    this.stomach.push(someFood);
+  } 
+
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  } 
+
+  
+  Person.prototype.toString = function(){
+    return `${this.name},${this.age}`
+  } 
+
 
   
   
@@ -63,11 +82,26 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = tank;
+    this.odometer = odometer;
+  }
+
+  const tank = 0;
+  const odometer = 0;
+
+  Car.prototype.fill = function(){
+    if(tank = 0){
+      return tank + 100;
+    }
   }
   
-  
+  const carModel = new Car("chevy", 20);
+  console.log(carModel);
+
+
   /*
     TASK 3
       - Write a Baby constructor subclassing Person.
@@ -75,18 +109,32 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+
+ function Baby(name,age,favoriteToy) {
+   this.name = name;
+   this.age = age;
+   this.favoriteToy = favoriteToy;
   }
- 
+
+  function Dad(name,age,favoriteToy) {
+    Baby.call(this,name,age,favoriteToy)
+   }
+
+  Baby.prototype.play = function(){
+    console.log(`Playing with ${this.favoriteToy}`);
+  }
+
+ const babySubPerson = new Dad('dad',45,'motorcycle');
+
+ Dad.prototype = Object.create(Baby.prototype);
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. if the this keyword is not called or nested then it will refer to the window as a global object.
+    2.imlplicit binding is where if your using a method on an object whatever is left of the dot will get the this keyword.
+    3. with constructors we use the new keyword to create a new object
+    4. explicit binding you can point to another object depending on which keyword you use (call apply)
   */
   
   
